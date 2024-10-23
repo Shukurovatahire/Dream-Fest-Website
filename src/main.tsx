72d1ff8861b86c4bd1e62c486fa54d285e792b7b
Shukurova Tahire <sukurovatahire08@gmail.com>
2024-10-24 00:05:50 +0400

    more changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,21 +3,23 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import AllArtists from "./Pages/AllArtists/AllArtists";
+import Concerts from "./Pages/Concerts/Concerts";
+import ConcertDetails from "./Pages/ConcertDetails/ConcertDetails";
+import ShoppingCart from "./Pages/ShoppingCart/ShoppingCart";
+import Order from "./Pages/Order/Order";
+import Payment from "./Pages/Payment/Payment";
+import Contact from "./Pages/Contact/Contact";
 import { I18nextProvider } from "react-i18next";
-import React, { lazy, Suspense } from "react";
+import React from "react";
 import i18n from "./i18n";
-
-// Lazy load for large components
-const AllArtists = lazy(() => import("./Pages/AllArtists/AllArtists"));
-const Concerts = lazy(() => import("./Pages/Concerts/Concerts"));
-const ConcertDetails = lazy(() => import("./Pages/ConcertDetails/ConcertDetails"));
-const ShoppingCart = lazy(() => import("./Pages/ShoppingCart/ShoppingCart"));
-const Order = lazy(() => import("./Pages/Order/Order"));
-const Payment = lazy(() => import("./Pages/Payment/Payment"));
-const Contact = lazy(() => import("./Pages/Contact/Contact"));
-const Login = lazy(() => import("./Pages/Login/Login"));
-const Profile = lazy(() => import("./Pages/Profile/Profile"));
+import Login from "./Pages/Login/Login";
+import Profile from "./Pages/Profile/Profile";
 
 const router = createBrowserRouter([
   {
@@ -26,82 +28,46 @@ const router = createBrowserRouter([
   },
   {
     path: "/allartists",
-    element: (
-      <Suspense fallback={<div>Loading...</div>}>
-        <AllArtists />
-      </Suspense>
-    ),
+    element: <AllArtists />,
   },
   {
     path: "/concerts",
-    element: (
-      <Suspense fallback={<div>Loading...</div>}>
-        <Concerts />
-      </Suspense>
-    ),
+    element: <Concerts />,
   },
   {
     path: "/concertdetail/:id",
-    element: (
-      <Suspense fallback={<div>Loading...</div>}>
-        <ConcertDetails />
-      </Suspense>
-    ),
+    element: <ConcertDetails />,
   },
   {
     path: "/cart",
-    element: (
-      <Suspense fallback={<div>Loading...</div>}>
-        <ShoppingCart />
-      </Suspense>
-    ),
+    element: <ShoppingCart />,
   },
   {
     path: "/order/:id",
-    element: (
-      <Suspense fallback={<div>Loading...</div>}>
-        <Order />
-      </Suspense>
-    ),
+    element: <Order />,
   },
   {
     path: "/payment",
-    element: (
-      <Suspense fallback={<div>Loading...</div>}>
-        <Payment />
-      </Suspense>
-    ),
+    element: <Payment />,
   },
   {
     path: "/contact",
-    element: (
-      <Suspense fallback={<div>Loading...</div>}>
-        <Contact />
-      </Suspense>
-    ),
+    element: <Contact />,
   },
   {
-    path: "/login",
-    element: (
-      <Suspense fallback={<div>Loading...</div>}>
-        <Login />
-      </Suspense>
-    ),
+    path:"/login",
+    element:<Login/>
   },
   {
-    path: "/profile/:id",
-    element: (
-      <Suspense fallback={<div>Loading...</div>}>
-        <Profile />
-      </Suspense>
-    ),
-  },
+    path:"/profile/:id",
+    element:<Profile/>
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <I18nextProvider i18n={i18n}>
-      <RouterProvider router={router} />
+        <RouterProvider router={router} />
     </I18nextProvider>
   </Provider>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import { defineConfig as defineVitestConfig } from 'vitest/config';
 import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // Alias ayarını yapıyoruz
-    },
+  server: {
+    open: true,
   },
-  server:{
-    open:true,
+test:{
+  globals:true,
+  environment:"jsdom",
+  setupFiles:"./src/setupTests.ts"
+}, resolve: {
+  alias: {
+    '@': path.resolve(__dirname, 'src'),
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-          if (id.includes('src/components')) {
-            return 'components';
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,
-  },
+},
 });
