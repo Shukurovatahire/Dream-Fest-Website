7771b6a92a27abf7f7637fea1a64b408d5638b04
Shukurova Tahire <sukurovatahire08@gmail.com>
2024-10-24 00:13:04 +0400

    more changes

--- dist/assets/Button-BOGBLznd.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./vendor-VF7iyE7l.js";const i=t=>{const{text:s,bgColor:r,color:n,onClick:e,disabled:l,className:a}=t;return o.jsx(o.Fragment,{children:o.jsx("button",{className:`btn ${a}`,disabled:l,onClick:e,style:{backgroundColor:r,color:n},children:s})})};export{i as B};
